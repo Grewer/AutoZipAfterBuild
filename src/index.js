25f63c84f8d0bf6ade23fd5ec0b5f566a10df019
Grewer <grewer@grewer.cn>
2018-10-26 10:56:18 +0800

    feat(module): 采用 传入 pwd  来获取文件

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ class AutoZipAfterBuild {
       compiler.plugin('done', (compilation) => {
         // webpack 3.8
         console.log(__dirname) // '/'
-        console.log('处于编译完成的阶段', compilation.compilation)
+        console.log('处于编译完成的阶段')
+        // 仍旧需要传入文件地址
+        // Object.keys(compilation.compilation.assets) // 可获取 assets  列表  但是不完全  因为没有 public 中的复制的文件和插件生成的文件
         // 因为 done 所以不需要 callback
       })
     }
